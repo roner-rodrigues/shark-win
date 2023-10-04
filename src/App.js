e0c759bd217b50a465293c7083e44e50e9ddba3c
Roner Rodrigues <ronerhenry@gmail.com>
2023-10-04 02:42:17 -0300

    great changes!

--- src/App.js
+++ src/App.js
@@ -1,46 +1,25 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './App.css'; 
+import React, { useState, useRef, useEffect } from 'react';
+import './App.css';
 
-const rootElement = document.getElementById('root');
-  
 function RepeatButton(props) {
-  return (
-    <button 
-      aria-label='Play again.' 
-      id='repeatButton' 
-      onClick={props.onClick}>
-    </button>
-  );
+    return (
+        <button 
+            aria-label='Play again.' 
+            id='repeatButton' 
+            onClick={props.onClick}>
+        </button>
+    );
 }
 
 function WinningSound() {
-  return (
-  <audio autoplay="autoPlay" className="player" preload="false">
-    <source src="https://andyhoffman.codes/random-assets/img/slots/winning_slot.wav" />
-  </audio>  
-  );
+    return (
+        <audio autoplay="autoplay" className="player" preload="false">
+            <source src="https://andyhoffman.codes/random-assets/img/slots/winning_slot.wav" />
+        </audio>
+    );
 }
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      winner: null
-    }
-    this.finishHandler = this.finishHandler.bind(this)
-    this.handleClick = this.handleClick.bind(this);
-  }  
-
-  handleClick() { 
-    this.setState({ winner: null });
-    this.emptyArray();
-    this._child1.forceUpdateHandler();
-    this._child2.forceUpdateHandler();
-    this._child3.forceUpdateHandler();
-  }
-
-  static loser = [
+const loser = [
     'Not quite', 
     'Stop gambling', 
     'Hey, you lost!', 
@@ -51,160 +30,164 @@ class App extends React.Component {
     'You\'re awesome at losing',
     'Coding is hard',
     'Don\'t hate the coder'
-  ];
+];
 
-  static matches = [];
+function App() {
+    const matches = useRef([]);
+    const [winner, setWinner]       = useState(false);
+    const [hasPlayed, setHasPlayed] = useState(false);
 
-  finishHandler(value) {
-    App.matches.push(value);  
+    const finishHandler = (value) => {
+        if (!hasPlayed) return;  
 
-    if (App.matches.length === 3) {
-      const { winner } = this.state;
-      const first = App.matches[0];
-      let results = App.matches.every(match => match === first)
-      this.setState({ winner: results });
+        matches.current.push(value);  
+        if (matches.current.length === 3 && hasPlayed) {
+            const first = matches.current[0];
+            let results = matches.current.every(match => match === first);
+            setWinner(results);
+            setHasPlayed(false);
+        }
     }
-  }
 
-  emptyArray() {
-    App.matches = [];
-  }
-
-  render() {
-    const { winner } = this.state;
-    const getLoser = () => {       
-      return App.loser[Math.floor(Math.random()*App.loser.length)]
+    const emptyArray = () => {
+        matches.current = [];
     }
-    let repeatButton = null;
-    let winningSound = null;
 
-    if (winner !== null) {
-      repeatButton = <RepeatButton onClick={this.handleClick} />
+    const handleClick = () => { 
+        setWinner(null);
+        emptyArray();
+        // child1.current.moveToIcon(5);
+        child1.current.forceUpdateHandler();
+        
+        child2.current.forceUpdateHandler();
+        child3.current.forceUpdateHandler();
+        setHasPlayed(true);
+
+    }
+
+    const getLoser = () => {
+        return loser[Math.floor(Math.random()*loser.length)];
+    }
+
+    const child1 = useRef(null);
+    const child2 = useRef(null);
+    const child3 = useRef(null);
+    
+    return (
+        <div>
+            {winner === true && <WinningSound />}
+            <h1 style={{ color: 'white'}}>
+                <span>{winner === null ? 'Waiting…' : winner ? '🤑 Pure skill! 🤑' : getLoser()}</span>
+            </h1>
+
+            <div className={`spinner-container`}>
+                <Spinner hasPlayed={hasPlayed} onFinish={finishHandler} ref={child1} timer="1000" />
+                <Spinner hasPlayed={hasPlayed} onFinish={finishHandler} ref={child2} timer="1400" />
+                <Spinner hasPlayed={hasPlayed} onFinish={finishHandler} ref={child3} timer="2200" />
+                <div className="gradient-fade"></div>
+            </div>
+            {winner !== null && <RepeatButton onClick={handleClick} />}
+        </div>
+    );
+}
+
+const Spinner = React.forwardRef((props, ref) => {
+    const [position, setPosition] = useState(0);
+    const [timeRemaining, setTimeRemaining] = useState(props.timer);
+    const iconHeight = 188;
+    const multiplier = Math.floor(Math.random()*(4-1)+1); 
+
+    const setStartPosition = () => {
+        return ((Math.floor((Math.random()*9))) * iconHeight)*-1;
+    }
+    const start = useRef(setStartPosition());
+
+    const getIconPositions = () => {
+        const iconHeight = 188;
+        const totalIcons = 9;
+        const positions = [];
+    
+        for(let i = 0; i < totalIcons; i++) {
+            positions.push(-i * iconHeight);
+        }
+    
+        return positions;
+    }
+    const iconPositions = getIconPositions();
+
+    const forceToIcon = (iconIndex) => {
+        if (iconIndex >= 0 && iconIndex < 9) { 
+            setPosition(iconPositions[iconIndex]);
+        }
+    }
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            tick();
+        }, 100);
+
+        return () => clearInterval(timer);
+    }, [timeRemaining, position]);
+
+    const tick = () => {
+        if (timeRemaining <= 0) {
+            stopSpinner();  
+            if (props.hasPlayed) {
+                getSymbolFromPosition();
+            }
+        } else {
+            moveBackground();
+        }      
+    }
+
+    const moveBackground = () => {
+        setPosition(position - iconHeight * multiplier);
+        setTimeRemaining(timeRemaining - 100);
+    }
+
+    const stopSpinner = () => {
+        let moved = (props.timer/100) * multiplier;
+        let startPosition = start.current;
+        let currentPosition = startPosition;
+        const totalSymbols = 9;
+        const maxPosition = (iconHeight * (totalSymbols-1)*-1);
+    
+        for (let i = 0; i < moved; i++) {              
+            currentPosition -= iconHeight;
+            if (currentPosition < maxPosition) {
+                currentPosition = 0;
+            }      
+        }
+    
+        setPosition(currentPosition);  // isso efetivamente para a roleta
     }
     
-    if (winner) {
-      winningSound = <WinningSound />
+    const getSymbolFromPosition = () => {
+        // props.onFinish(position);
+
+        const totalSymbols = 9;
+        // Esta linha de código determina qual ícone está na janela visível, dado a posição final.
+        let index = Math.abs(position) % (iconHeight * totalSymbols);
+        index = Math.floor(index / iconHeight);
+        props.onFinish(index);
     }
 
-    return (
-      <div>
-        {winningSound}
-        <h1 style={{ color: 'white'}}>
-          <span>{winner === null ? 'Waiting…' : winner ? '🤑 Pure skill! 🤑' : getLoser()}</span>
-        </h1>
 
-        <div className={`spinner-container`}>
-          <Spinner onFinish={this.finishHandler} ref={(child) => { this._child1 = child; }} timer="1000" />
-          <Spinner onFinish={this.finishHandler} ref={(child) => { this._child2 = child; }} timer="1400" />
-          <Spinner onFinish={this.finishHandler} ref={(child) => { this._child3 = child; }} timer="2200" />
-          <div className="gradient-fade"></div>
-        </div>
-        {repeatButton}          
-      </div>
-    );
-  }
-}  
-  
-class Spinner extends React.Component {  
-  constructor(props){
-    super(props);
-    this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
-  };
-
-  forceUpdateHandler(){
-    this.reset();
-  }; 
-
-  reset() {
-    if (this.timer) { 
-      clearInterval(this.timer); 
-    }  
-
-    this.start = this.setStartPosition();
-
-    this.setState({
-      position: this.start,
-      timeRemaining: this.props.timer        
-    });
-
-    this.timer = setInterval(() => {
-      this.tick()
-    }, 100);      
-  }
-
-  state = {
-    position: 0,
-    lastPosition: null
-  }
-  static iconHeight = 188;
-  multiplier = Math.floor(Math.random()*(4-1)+1);
-
-  start = this.setStartPosition();
-  speed = Spinner.iconHeight * this.multiplier;    
-
-  setStartPosition() {
-    return ((Math.floor((Math.random()*9))) * Spinner.iconHeight)*-1;
-  }
-
-  moveBackground() {
-    this.setState({ 
-      position: this.state.position - this.speed,
-      timeRemaining: this.state.timeRemaining - 100
-    })
-  }
-
-  getSymbolFromPosition() {
-    let { position } = this.state;
-    const totalSymbols = 9;
-    const maxPosition = (Spinner.iconHeight * (totalSymbols-1)*-1);
-    let moved = (this.props.timer/100) * this.multiplier
-    let startPosition = this.start;
-    let currentPosition = startPosition;    
-
-    for (let i = 0; i < moved; i++) {              
-      currentPosition -= Spinner.iconHeight;
-
-      if (currentPosition < maxPosition) {
-        currentPosition = 0;
-      }      
-    }
-
-    this.props.onFinish(currentPosition);
-  }
-
-  tick() {      
-    if (this.state.timeRemaining <= 0) {
-      clearInterval(this.timer);        
-      this.getSymbolFromPosition();    
-
-    } else {
-      this.moveBackground();
-    }      
-  }
-
-  componentDidMount() {
-    clearInterval(this.timer);
-
-    this.setState({
-      position: this.start,
-      timeRemaining: this.props.timer
-    });
-
-    this.timer = setInterval(() => {
-      this.tick()
-    }, 100);
-  }
-
-  render() {
-    let { position, current } = this.state;   
+    React.useImperativeHandle(ref, () => ({
+        forceUpdateHandler() {
+            start.current = setStartPosition();
+            setPosition(start.current);
+            setTimeRemaining(props.timer);
+        },
+        moveToIcon: forceToIcon
+    }));
 
     return (            
-      <div 
-        style={{backgroundPosition: '0px ' + position + 'px'}}
-        className={`icons`}          
-      />
+        <div 
+            style={{backgroundPosition: '0px ' + position + 'px'}}
+            className={`icons`}          
+        />
     )
-  }
-}
+});
 
 export default App;
